b53efa846d057a5cba96449eec3b111480093750
safiqul2022 <safiqulindia@gmail.com>
2022-02-21 00:14:39 +0530

    added addData method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,13 @@
 
 public class App {
 
+	public void addData(String data) {
+		System.out.println("data="+data);
+		if(data.equals("Safiqul")) {
+			System.out.println("Welcome");
+		}
+	}
+	
 	public static void main(String[] args) {
 
 		System.out.println("Hello World");
